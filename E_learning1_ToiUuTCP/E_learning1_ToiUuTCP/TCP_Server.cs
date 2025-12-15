a90ae38a200c395138d2021457d3fefe562baeb2
Khoa <khoap3105@gmail.com>
2025-12-15 17:05:29 +0700

    TCP bất đồng bộ

---
+++ E_learning1_ToiUuTCP/E_learning1_ToiUuTCP/TCP_Server.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TCP_Server
+{
+    internal class TCP_Server
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            string severHost = "127.0.0.1";
+            int serverPort = 5000;
+            var listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port: serverPort);
+            listener.Start();
+            Console.WriteLine($"Đang chờ kết nói đến {severHost} với port:{serverPort}");
+
+            while (true)
+            {
+                var client = await listener.AcceptTcpClientAsync();
+                Console.WriteLine("Đã có kết nối từ " + client.Client.RemoteEndPoint);
+                _ = HandleClientAsync(client);
+            }
+
+        }
+        private static async Task HandleClientAsync(TcpClient client)
+        {
+
+            using (client)
+            using (NetworkStream stream = client.GetStream())
+            {
+                //đọc tin nhắn từ client
+                byte[] buffer = new byte[1024];
+                try
+                {
+                    while (true) 
+                    {
+                        int byteRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    if (byteRead == 0)
+                    {
+                        Console.WriteLine("Client đã ngắt kết nối.");
+                        break;
+                    }
+                    string data = Encoding.UTF8.GetString(buffer, 0, byteRead);
+                    Console.WriteLine("Tin nhắn từ Client: " + data);
+
+                    // gửi phản hồi đến client
+                    String response = "Chào mừng đến với Server";
+                    byte[] responseData = Encoding.UTF8.GetBytes(response);
+                    await stream.WriteAsync(responseData, 0, responseData.Length);
+                    Console.WriteLine("Đang gửi phản hồi đến Client: " + response);
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("Kết nối lỗi/đã bị ngắt: " + ex.Message);
+
+                }
+
+            }
+            Console.WriteLine("Server quay lại chờ kết nối mới...");
+        }
+    }
+}
---
+++ E_learning1_ToiUuTCP/TCP_Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace TCP_Client
+{
+    internal class TCP_Client
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            string clientHost = "127.0.0.1";
+            int clientPort = 5000;
+            var client = new TcpClient();
+            client.Connect(IPAddress.Parse(clientHost), clientPort);
+            Console.WriteLine($"Đã kết nối đến server {clientHost} với port:{clientPort}");
+
+            using NetworkStream stream = client.GetStream();
+            while (true)
+            {
+                // Gửi tin nhắn đến server
+                Console.WriteLine("Gửi lời nhắn đến server");
+                String message = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                await stream.WriteAsync(data, 0, data.Length);
+
+                // Nhận phản hồi từ server
+                byte[] buffer = new byte[1024];
+                int byteRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                string response = Encoding.UTF8.GetString(buffer, 0, byteRead);
+                Console.WriteLine("Phản hồi từ Server: " + response);
+            }
+        }
+    }
+
+}
