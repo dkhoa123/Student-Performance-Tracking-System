e5fc7f5fc2a5d90b87614a6d3d57fbe915d41ca6
Khoa <khoap3105@gmail.com>
2025-12-24 18:46:11 +0700

    Xóa bản mẫu

--- StudentPerformanceTrackingSystem/StudentPerformanceTrackingSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentPerformanceTrackingSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
